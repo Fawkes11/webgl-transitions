4139a36e0aaccdf26ffe7991d32d1b904b30a617
Hessler Inampuez <hesslerinampuez@gmail.com>
2024-04-27 02:13:29 -0500

    images are added and the correct positions are confugured to match within the canvas with img tag

--- js/shaders/vertex.glsl
+++ js/shaders/vertex.glsl
@@ -18,8 +18,8 @@ void main() {
 
     vec4 defaultState = modelMatrix * vec4(position, 1.0);
     vec4 fullScreenState = vec4(position, 1.0);
-    fullScreenState.x *= uResolution.x/uQuadSize.x;
-    fullScreenState.y *= uResolution.y/uQuadSize.y;
+    fullScreenState.x *= uResolution.x;
+    fullScreenState.y *= uResolution.y;
 
     float cornersProgress = mix(
         mix(uCorners.z, uCorners.w, uv.x),
@@ -27,9 +27,9 @@ void main() {
         uv.y
     );
 
-    vec4 finalState = mix(defaultState,fullScreenState, uProgress + waves);
+    vec4 finalState = mix(defaultState,fullScreenState, cornersProgress);  /* uProgress + waves */
 
-    vSize = mix(uQuadSize, uResolution, uProgress);
+    vSize = mix(uQuadSize, uResolution, cornersProgress); /* uProgress */
 
     gl_Position = projectionMatrix * viewMatrix * finalState;
 }
